16539281f7f921c00732626d41c7a23321fa43a5
devtechsolution <srivastvadevzone@gamil.com>
2020-12-01 16:20:32 +0530

    Implemented config server

---
+++ spring-config-server/src/test/java/org/as/devtechsolution/config/server/SpringConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.as.devtechsolution.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
